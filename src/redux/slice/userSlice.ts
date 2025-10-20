2bbb1efefd1d2c3c4572a24fe3c243899b3bd39f
Yaroslav Sadovskyi <111430842+Yamy007@users.noreply.github.com>
2025-10-20 22:38:24 +0300

    Remove unused import isRejectedWithValue

--- src/redux/slice/userSlice.ts
+++ src/redux/slice/userSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, isRejectedWithValue } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { userService } from "../../services/apiUserService";
 import { AxiosError } from "axios";
 import { IUser } from "../../interface/user";
